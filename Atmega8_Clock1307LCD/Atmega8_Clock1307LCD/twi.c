5385991c41389f14ce1c0ae812c22e823b4ddd7b
alex <o.lototskyi@gmail.com>
2023-03-19 14:03:10 +0200

    clock atmega8, DS1307 and LCD (1602)

---
+++ Atmega8_Clock1307LCD/Atmega8_Clock1307LCD/twi.c
@@ -0,0 +1,46 @@
+#include "twi.h"
+
+void I2C_Init(void)
+{
+	TWBR = 0x20; // frequency of I2C (if 8MHz of MC then 100kHz) [TWBR = ((F_CPU/F_SCL) - 16)/(2 * 4^TWPS))]
+}
+
+void I2C_StartCondition(void)
+{
+	TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
+	while(!(TWCR & (1 << TWINT))); //waiting until TWINT is set
+}
+
+void I2C_StopCondition(void)
+{
+	TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
+}
+
+void I2C_SendByte(unsigned char c)
+{
+	TWDR = c; //write byte to data register
+	TWCR = (1 << TWINT) | (1 << TWEN); //turn on the data transfer
+	while(!(TWCR & (1 << TWINT))); //waiting until TWINT is set
+}
+
+void I2C_SendByteByADDR(unsigned char c, unsigned char addr)
+{
+	I2C_StartCondition();
+	I2C_SendByte(addr);
+	I2C_SendByte(c);
+	I2C_StopCondition();
+}
+
+unsigned char I2C_ReadByte(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWEN)|(1<<TWEA);
+	while (!(TWCR & (1<<TWINT))); //waiting TWIN
+	return TWDR; //read data register
+}
+
+unsigned char I2C_ReadLastByte(void)
+{
+	TWCR = (1<<TWINT)|(1<<TWEN);
+	while (!(TWCR & (1<<TWINT))); //waiting TWIN
+	return TWDR; //read data register
+}
