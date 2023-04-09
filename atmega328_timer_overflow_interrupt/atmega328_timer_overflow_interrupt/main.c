0549a4c89e7d05e1abcb977fe32ba0588ca25f11
alex <o.lototskyi@gmail.com>
2023-04-09 17:48:16 +0300

    test fast PWM and Phase Correct PWM Mode, test external interrupt

---
+++ atmega328_timer_overflow_interrupt/atmega328_timer_overflow_interrupt/main.c
@@ -0,0 +1,37 @@
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+#include <avr/interrupt.h>
+
+#define timer_on TCCR1B |= (1 << CS12) | (1 << CS10);
+#define timer_off TCCR1B &= ~((1 << CS12) | (1 << CS10));
+
+ISR (TIMER1_OVF_vect) {
+    timer_off;
+    PORTC |= (1 << PC5);
+    _delay_ms(200);
+    PORTC &= ~(1 << PC5);
+    _delay_ms(100);
+    timer_on;
+}
+
+int main(void)
+{
+    DDRC = 0xFF;
+    PORTC = 0x00;
+    
+    timer_on;
+    TIMSK1 |= (1 << TOIE1);
+    TCNT1 = 0;
+    sei();
+    while (1) 
+    {
+        for (int i = 0; i < 4; i++) {
+            PORTC |= (1 << i);
+            _delay_ms(200);
+            PORTC &= ~(1 << i);
+        }
+        _delay_ms(3);
+    }
+}
+
--- atmega328p-test/atmega328p-test/main.c
+++ atmega328p-test/atmega328p-test/main.c
@@ -6,9 +6,10 @@
 int main(void)
 {
 
-    DDRB |=  (1<<PORTB1); //Define PORTB1 (PIN15) as Output
-    TCCR1A |= (1<<COM1A1) | (1<<WGM10);  //Set Timer Register
-    TCCR1B |= (1<<WGM12) | (1<<CS11);
+    DDRB |=  (1 << PORTB1); //Define PORTB1 (PIN15) as Output
+    PORTB &= ~(1 << PORTB1);
+    TCCR1A |= (1 << COM1A1) | (1 << WGM11) | (1 << WGM10);  //Set Timer Register
+    TCCR1B |= (1<<CS11);
     OCR1A = 0;
     
     unsigned char pwm_state = 0;
@@ -17,7 +18,7 @@ int main(void)
     {
 		if (pwm_state == 0) {
     		OCR1A++;
-    		if (OCR1A > 254) {
+    		if (OCR1A >= 1023) {
         		pwm_state = 1;
     		}
 		}
