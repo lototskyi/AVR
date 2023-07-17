6079103959e9ee8dc09b3192d200e07b0f443bf1
alex <o.lototskyi@gmail.com>
2023-07-17 21:28:58 +0300

    macros for delay, using stack to preserve data of registers

--- AssemblerDelay/AssemblerDelay/main.asm
+++ AssemblerDelay/AssemblerDelay/main.asm
@@ -1,8 +1,9 @@
+    .include "DelayMacro.inc"
     .cseg
     .org    0x00
     .def    mask = r16      ; mask register
     .def    ledR = r17      ; led register
-    .def    loopCt = r18    ; delay loop count
+    ;.def    loopCt = r18    ; delay loop count
 
     ; initialize stack pointer to RAMEND
     ldi     r16, LOW(RAMEND); store lower bits of last RAM address
@@ -18,8 +19,10 @@ start:
     eor     ledR, mask
     out     PORTD, ledR
 
-    ldi     loopCt, 50      ; initialize delay multiple for 0.5 sec
-    rcall   delay10ms       ; call delay subroutine
+    ;ldi     loopCt, 50      ; initialize delay multiple for 0.5 sec
+    ;rcall   delay10ms       ; call delay subroutine
+
+    delayms 500;
 
     rjmp    start
 
